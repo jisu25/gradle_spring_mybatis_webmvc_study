b7878bbf96b2b2849e593456c5b45e762fa17b2c
jisu25 <kimpigu.zz@gmail.com>
2020-10-28 11:17:04 +0900

    init

--- src/main/java/gradle_spring_mybatis_webmvc_sutdy/config/ContextSqlSession.java
+++ src/main/java/gradle_spring_mybatis_webmvc_sutdy/config/ContextSqlSession.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@MapperScan(basePackages = {"gradle_spring_db_study.mapper"})
+@MapperScan(basePackages = {"gradle_spring_mybatis_webmvc_study.mapper"})
 public class ContextSqlSession {
 
 	@Autowired
@@ -26,7 +26,7 @@ public class ContextSqlSession {
 		
 		factoryBean.setDataSource(dataSource);
 		factoryBean.setConfigLocation(ApplicationContext.getResource("classpath:/mybatis-config.xml"));
-//		factoryBean.setMapperLocations(ApplicationContext.getResources("classpath:/mappers/*Mapper.xml"));
+		factoryBean.setMapperLocations(ApplicationContext.getResources("classpath:/mappers/*Mapper.xml"));
 		
 		return factoryBean;
 	}
